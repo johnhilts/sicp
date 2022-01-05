a79eb448d67b07be868d052f7cfae4664ab0e462
John Hilts <johnhilts@live.com>
2022-01-04 18:44:36 -0800

    sicp - Exercise 1.24

--- ch1/1.24.exercise.scm
+++ ch1/1.24.exercise.scm
@@ -1,34 +1,31 @@
-(define (next n)
-  (cond
-   ((= 2 n)
-    3)
-   (else (+ 2 n))))
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
 
 (define (square n)
   (* n n))
 
-(define (smallest-divisor n)
-  (find-divisor n 2))
-
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (next test-divisor)))))
-
-(define (divides? a b)
-  (= (remainder b a) 0))
-
-(define (prime? n)
-  (= n (smallest-divisor n)))
-
 (define (timed-prime-test n)
-  ;; (newline)
-  ;; (display n)
   (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
   (cond
-   ((prime? n)
+   ((fast-prime? n 1)
     (report-prime (- (runtime) start-time))
     #t)
    (else #f)))
