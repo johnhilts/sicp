ceca61bbe7663559fa047bc223312b7ca6eff39a
John Hilts <johnhilts@live.com>
2022-01-10 23:09:22 -0800

    sicp - Compose and Repeated (Chapter 1)

---
+++ ch1/1.43.exercise.scm
@@ -0,0 +1,11 @@
+;; ((compose square inc) 6)
+;; ((repeated square 2) 5)
+(define (repeated f n)
+  (cond
+   ((= n 1)
+    (lambda (x)
+      (f x)))
+   (else (compose ;; remember, compose just takes 2 functions, that's it!
+          f
+          (repeated f (- n 1))))))
+
